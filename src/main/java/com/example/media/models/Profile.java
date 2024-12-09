c444f3d377f4fc467ba2bf711ba1648036877377
Sr1jan27 <singhsrijan809@gmail.com>
2024-12-09 08:08:45 +0530

    defining models and there anottations

--- src/main/java/com/example/media/models/Profile.java
+++ src/main/java/com/example/media/models/Profile.java
@@ -1,4 +1,12 @@
 package com.example.media.models;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Profile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @OneToOne
+    private User user;
 }
