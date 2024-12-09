026ce2042f1e1e7a57412c540ba65fad7da00088
Sr1jan27 <singhsrijan809@gmail.com>
2024-12-09 08:24:48 +0530

    trying to make user owner of the one to one relationship

--- src/main/java/com/example/media/models/SocialProfile.java
+++ src/main/java/com/example/media/models/SocialProfile.java
@@ -7,7 +7,7 @@ public class SocialProfile {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @OneToOne
-    @JoinColumn(name = "social_user")
+    @OneToOne(mappedBy = "socialProfile")
+//    @JoinColumn(name = "social_user")
     private SocialUser user;
 }
